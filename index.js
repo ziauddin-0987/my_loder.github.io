abecca5c88a847f0d341d42b3af020b1f7a7473e
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-30 23:40:00 +0500

    this is my loding

---
+++ index.js
@@ -0,0 +1,11 @@
+const my_loder = document.getElementById("loder");
+
+setTimeout(() => {
+  {
+    my_loder.style.display = "none";
+  }
+}, 3000);
+
+// function start_lod() {
+//   my_loder.style.display = "block";
+// }
